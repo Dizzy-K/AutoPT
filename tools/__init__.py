d7b48b83ca9a7e19c5fb5ea5ceb2f88f9a5a545b
mashiro01 <m1904727480@outlook.com>
2026-06-01 15:47:59 +0800

    style: ensure trailing newline

--- tools/__init__.py
+++ tools/__init__.py
@@ -8,6 +8,7 @@ from .registry import ToolSpec, build_default_tool_specs
 from .terminal import InteractiveShell, ShellCommandResult
 from .web import read_html
 
+
 __all__ = [
     "InteractiveShell",
     "ShellCommandResult",
